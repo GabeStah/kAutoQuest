0ad0c6e4a3c44f311a922ca5fe0428540c7a40dd
Gabe Wyatt <gabe@shoreline.io>
2022-09-27 00:03:45 -0700

    DF update

--- kAutoQuest.lua
+++ kAutoQuest.lua
@@ -1,6 +1,7 @@
 local addon = CreateFrame('Frame')
-addon.completed_quests = {}
-addon.uncompleted_quests = {}
+local DEBUG = false
+addon.complete_quests = {}
+addon.incomplete_quests = {}
 
 function addon:canAutomate ()
 	if IsShiftKeyDown() then
@@ -21,6 +22,7 @@ end
 
 function addon:QUEST_PROGRESS ()
 	if not self:canAutomate() then return end
+  self:Debug('On QUEST_PROGRESS')
 	if IsQuestCompletable() then
 		CompleteQuest()
 	end
@@ -28,81 +30,105 @@ end
 
 function addon:QUEST_LOG_UPDATE ()
 	if not self:canAutomate() then return end
-	local start_entry = GetQuestLogSelection()
-	local num_entries = GetNumQuestLogEntries()
+  self:Debug('On QUEST_LOG_UPDATE')
+	local start_entry = C_QuestLog.GetSelectedQuest()
+	local num_entries = C_QuestLog.GetNumQuestLogEntries()
 	local title
 	local is_complete
 	local no_objectives
-
-	self.completed_quests = {}
-	self.uncompleted_quests = {}
+  local questId
 
 	if num_entries > 0 then
 		for i = 1, num_entries do
-			SelectQuestLogEntry(i)
-			title, _, _, _, _, _, is_complete = GetQuestLogTitle(i)
+			C_QuestLog.SetSelectedQuest(i)
+			title, _, questId = C_QuestLog.GetInfo(i)
+      is_complete = (questId and C_QuestLog.IsComplete(questId)) or false
 			no_objectives = GetNumQuestLeaderBoards(i) == 0
-			if title and (is_complete or no_objectives) then
-				self.completed_quests[title] = true
-			else
-				self.uncompleted_quests[title] = true
-			end
 		end
 	end
 
-	SelectQuestLogEntry(start_entry)
+	C_QuestLog.SetSelectedQuest(start_entry)
 end
 
 function addon:GOSSIP_SHOW ()
 	if not self:canAutomate() then return end
+  self:Debug('On GOSSIP_SHOW')
 
-	local button
-	local text
+  local activeQuests = C_GossipInfo.GetActiveQuests()
+  for index, quest in ipairs(activeQuests) do
+    if quest.isComplete then
+      C_GossipInfo.SelectActiveQuest(quest.questID)
+    end
+  end
 
-	for i = 1, 32 do
-		button = _G['GossipTitleButton' .. i]
-		if button:IsVisible() then
-			text = self:strip_text(button:GetText())
-			ABCDE={button:GetText(), text}
-			if button.type == 'Available' then
-				button:Click()
-			elseif button.type == 'Active' then
-				if self.completed_quests[text] then
-					button:Click()
-				end
-			end
-		end
-	end
+  local options = C_GossipInfo.GetOptions()
+  for index, gossipInfo in ipairs(options) do
+    if (strfind(gossipInfo.name, "(Quest)")) then
+      C_GossipInfo.SelectOption(gossipInfo.gossipOptionID)
+    end
+  end
+
+  local availableQuests = C_GossipInfo.GetAvailableQuests()
+  for index, quest in ipairs(availableQuests) do
+    if (not quest.isTrivial) and (not quest.repeatable) then
+      C_GossipInfo.SelectAvailableQuest(quest.questID)
+    end
+  end
 end
 
 function addon:QUEST_GREETING (...)
 	if not self:canAutomate() then return end
+  self:Debug('On QUEST_GREETING')
 
-	local button
-	local text
-
-	for i = 1, 32 do
-		button = _G['QuestTitleButton' .. i]
-		if button:IsVisible() then
-			text = self:strip_text(button:GetText())
-			if self.completed_quests[text] then
-				button:Click()
-			elseif not self.uncompleted_quests[text] then
-				button:Click()
-			end
+  for index=1, GetNumActiveQuests() do
+		local quest, isComplete = GetActiveTitle(index)
+		if isComplete then
+			SelectActiveQuest(index)
 		end
 	end
 end
 
+-- function addon:IsAppropriate(name, byCache)
+--   local daily
+--   if byCache then
+--       daily = (not not self.questCache[name])
+--   else
+--     -- for some reason questInfo in gossip table return data different from one from QuestCache
+--     local questID = GetQuestID()
+--     local qn = name or (questID and QuestCache:Get(questID).title or "")
+--     daily = QuestIsDaily() or QuestIsWeekly() or (not not self.questCache[qn])
+--   end
+
+--   return self:_isAppropriate(daily)
+-- end
+
+function addon:MarkQuest(id, completed)
+  if completed then
+    self.complete_quests[id] = true
+  else 
+    self.incomplete_quests[id] = true
+  end
+end
+
+function addon:IsQuestComplete(id)
+  return self.complete_quests[id]
+end
+
+function addon:IsQuestIncomplete(id)
+  return self.incomplete_quests[id]
+end
+
 function addon:QUEST_DETAIL ()
 	if not self:canAutomate() then return end
+  self:Debug('On QUEST_DETAIL')
 	AcceptQuest()
 end
 
 function addon:QUEST_COMPLETE (event)
 	if not self:canAutomate() then return end
+  self:Debug('On QUEST_COMPLETE')
 	if GetNumQuestChoices() <= 1 then
-		GetQuestReward(QuestFrameRewardPanel.itemChoice)
+		GetQuestReward(1)
 	end
 end
 
@@ -110,6 +136,12 @@ function addon.onevent (self, event, ...)
 	if self[event] then
 		self[event](self, ...)
 	end
+end
+
+function addon:Debug(...)
+  if DEBUG then
+    print(...)
+  end
 end
 
 addon:SetScript('OnEvent', addon.onevent)
